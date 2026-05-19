94fbd8edabd4b4662db5517f301e46770c3e1842
Prabhjot <prabhjotk0187@gmail.com>
2026-05-19 12:36:06 -0500

    Create Dialog function component with props parameter

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -1,7 +1,11 @@
 
-function Dialog() {
+function Dialog(props) {
   return (
-   <></>
+    <div className="dialog-overlay">
+            <div className="dialog-box">
+                <h2>Thank You</h2>
+            </div>
+        </div>
   );
 }
 
