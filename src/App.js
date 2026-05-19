6f30f8ecc092cb8df277afef53fed029d19c67e5
Prabhjot <prabhjotk0187@gmail.com>
2026-05-19 12:43:55 -0500

    create app component for dialog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import { useState } from "react";
 import StarRating from "./components/StarRating";
 import Dialog from "./components/Dialog";
 function App() {
+      const [showDialog, setShowDialog] = useState(false);
+    
+    const closeDialog = () => {
+        setShowDialog(false);
+    };
   return (
    <></>
   );
