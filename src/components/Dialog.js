3f6b94893c1e83df68d117a37dad5049ad768d27
Prabhjot <prabhjotk0187@gmail.com>
2026-05-19 12:38:14 -0500

    add dialog content and close button with onclick

--- src/components/Dialog.js
+++ src/components/Dialog.js
@@ -4,6 +4,11 @@ function Dialog(props) {
     <div className="dialog-overlay">
             <div className="dialog-box">
                 <h2>Thank You</h2>
+                <p>Your rating has been submitted successfully.</p>
+                <button 
+                    className="dialog-btn" 
+                    onClick={props.closeDialog}>Close
+                </button>
             </div>
         </div>
   );
