72acdaf492df9bccbca3beb6137a50c62276b8e8
Prabhjot <prabhjotk0187@gmail.com>
2026-05-19 12:28:57 -0500

    import star component and define rating words object

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -1,5 +1,16 @@
 import { useState } from "react";
+import Star from "./Star";
+
 function StarRating() {
+    const [rating, setRating] = useState(0);
+
+    const ratingWords = {
+        1: 'Poor',
+        2: 'Fair', 
+        3: 'Good',
+        4: 'Very Good',
+        5: 'Excellent'
+    };
   return (
    <></>
   );
